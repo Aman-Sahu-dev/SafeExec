b1d581d4cf56f190aa5c128cf1111a2441e3e71d
Aman Sahu <amansahu3369@gmail.com>
2026-08-02 19:38:52 +0530

    outline all the phases and write main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use nix::libc;
+use safeexec::cli::{Args, TheaterMode};
+use safeexec::error::Result;
+use safeexec::runtime::RuntimeOrchestrator;
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    if unsafe { libc::geteuid() } != 0 {
+        eprintln!("warning:  safeexec requires root or Cap_SYS_ADMIN for namspace operations. ");
+
+        let orchastrator = RuntimeOrchestrator::new(args)?;
+
+        match orchastrator.run() {
+            OK(()) => {
+                println!("Safeexec completed successfully");
+                OK(())
+            }
+            Err(e) => {
+                eprintln!("x SafeExec failed{}", e);
+                Err(e)
+            }
+        }
+    }
 }
