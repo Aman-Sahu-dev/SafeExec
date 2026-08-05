7ff15a7893962c0795fc83958bbe40cc847b16b0
Aman Sahu <amansahu3369@gmail.com>
2026-08-05 19:24:32 +0530

    added runtime.rs

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,1 +1,22 @@
+use std::fmt::Result;
 
+use crate::cli::Args;
+use crate::error::Result;
+
+pub struct RuntimeOrchesterator {
+    pub args: Args,
+    pub session_id: String,
+}
+
+impl RuntimeOrchesterator {
+    pub fn new(args: Args) -> Result<Self> {
+        let session_id = args
+            .session_id
+            .clone()
+            .unwrap_or_else(|| nanoid::nanoid!(10));
+        Ok(Self { args, session_id })
+    }
+}
+pub fn run(&self) -> Result<()> {
+    todo!("RuntimeOrchesterator::run() - integrate later");
+}
