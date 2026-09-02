58221683426182da6707eeabd1be0f3d52220a1a
Aman-Sahu-dev <amansahu3369@gmail.com>
2026-09-02 20:15:21 +0530

    fixed all of rust ownerships rules issues in sync_primitives and make and going to keep nix latest version

--- src/sync_primitives.rs
+++ src/sync_primitives.rs
@@ -16,48 +16,44 @@ impl SyncBarrier {
             p2c: [p2c_r, p2c_w],
         })
     }
-    pub fn child_view(&self) -> ChildBarrier {
+    pub fn child_view(self) -> ChildBarrier {
+        let [_, c2p_w] = self.c2p;
+        let [p2c_r, _] = self.p2c;
         ChildBarrier {
-            c2p_read: self.c2p[0],
-            c2p_write: self.c2p[1],
-            p2c_read: self.p2c[0],
-            p2c_write: self.p2c[1],
+            c2p_write: c2p_w,
+            p2c_read: p2c_r,
         }
     }
-    pub fn parent_view(&self) -> ParentBarrier {
+    pub fn parent_view(self) -> ParentBarrier {
+        let [_, p2c_w] = self.p2c;
+        let [c2p_r, _] = self.c2p;
         ParentBarrier {
-            c2p_read: self.c2p[0],
-            c2p_write: self.c2p[1],
-            p2c_read: self.p2c[0],
-            p2c_write: self.p2c[1],
+            c2p_read: c2p_r,
+            p2c_write: p2c_w,
         }
     }
 }
 pub struct ChildBarrier {
-    c2p_read: OwnedFd,
     c2p_write: OwnedFd,
     p2c_read: OwnedFd,
-    p2c_write: OwnedFd,
 }
 impl ChildBarrier {
-    pub fn signal_ready(&self) -> Result<()> {
+    pub fn signal_ready(self) -> Result<()> {
         let _ = write(self.c2p_write, &[1])?;
         Ok(())
     }
-    pub fn wait_for_parent_continue(&self) -> Result<()> {
+    pub fn wait_for_parent_continue(self) -> Result<()> {
         let mut buf = [0u8; 1];
         let _ = read(self.p2c_read, &mut buf)?;
         Ok(())
     }
-    pub fn close_parent_descriptors(&self) -> Result<()> {
-        close(self.c2p_read)?;
-        close(self.p2c_write)?;
+    pub fn close_parent_descriptors(self) -> Result<()> {
+        close(self.c2p_write)?;
+        close(self.p2c_read)?;
         Ok(())
     }
 }
 pub struct ParentBarrier {
     c2p_read: OwnedFd,
-    c2p_write: OwnedFd,
-    p2c_read: OwnedFd,
     p2c_write: OwnedFd,
 }
