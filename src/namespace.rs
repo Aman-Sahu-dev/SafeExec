172adf46560900897ef648cb067e555f9c7581d2
Aman Sahu <amansahu3369@gmail.com>
2026-09-04 19:37:32 +0530

    fixed a lot of errors in namespace.rs and carefully write down according to rust 2024 rules

--- src/namespace.rs
+++ src/namespace.rs
@@ -1,4 +1,4 @@
-use crate::error::{Result, SafeExecError};
+use crate::error::Result;
 use nix::sched::{CloneFlags, clone};
 use nix::unistd::Pid;
 
@@ -8,4 +8,24 @@ impl NamespaceController {
     pub fn new() -> Self {
         Self
     }
+    pub fn build_clone_flags() -> CloneFlags {
+        CloneFlags::CLONE_NEWPID
+            | CloneFlags::CLONE_NEWNS
+            | CloneFlags::CLONE_NEWUTS
+            | CloneFlags::CLONE_NEWUSER
+            | CloneFlags::CLONE_NEWNET
+    }
+    pub unsafe fn spawn_container_init<F>(
+        &self,
+        flags: CloneFlags,
+        stack: &mut [u8],
+        callback: F,
+    ) -> Result<Pid>
+    where
+        F: FnMut() -> isize,
+    {
+        let pid = unsafe { clone(Box::new(callback), stack, flags, Some(nix::libc::SIGCHLD))? };
+        Ok(pid)
+    }
+    pub fn write_uid_gid_map(&self, pid: Pid, host_uid: u32, host_gid: u32) {}
 }
