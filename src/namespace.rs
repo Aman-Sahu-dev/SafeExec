5e54e6ed70aaa80f63b707fabae8611a7a65fdcd
Aman Sahu <amansahu3369@gmail.com>
2026-09-06 20:21:32 +0530

    finished namespaces.rs and syntax probelms

--- src/namespace.rs
+++ src/namespace.rs
@@ -1,4 +1,4 @@
-use crate::error::Result;
+use crate::error::{Result, SafeExecError};
 use nix::sched::{CloneFlags, clone};
 use nix::unistd::Pid;
 
@@ -27,5 +27,20 @@ impl NamespaceController {
         let pid = unsafe { clone(Box::new(callback), stack, flags, Some(nix::libc::SIGCHLD))? };
         Ok(pid)
     }
-    pub fn write_uid_gid_map(&self, pid: Pid, host_uid: u32, host_gid: u32) {}
+    pub fn write_uid_gid_map(&self, pid: Pid, host_uid: u32, host_gid: u32) -> Result<()> {
+        let uid_map = format!("0 {} 1\n", host_uid);
+        let gid_map = format!("0 {} 1\n", host_gid);
+
+        let uid_path = format!("/proc/{}/uid_map", pid);
+        let gid_path = format!("/proc/{}/gid_map", pid);
+
+        std::fs::write(&uid_path, uid_map)
+            .map_err(|e| SafeExecError::Namespace(format!("failed to write uid map: {}", e)));
+        let setgroups_path = format!("/proc/{}/setgroups", pid);
+        let _ = std::fs::write(setgroups_path, "deny");
+
+        std::fs::write(&gid_path, gid_map)
+            .map_err(|e| SafeExecError::Namespace(format!("failed to write gid_map {}", e)));
+        Ok(())
+    }
 }
